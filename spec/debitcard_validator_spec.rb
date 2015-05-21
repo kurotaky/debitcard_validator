c1185f4c6e448b632a8f8c7339ec52d606b5e600
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2015-05-21 15:41:15 +0900

    update spec messages

--- spec/debitcard_validator_spec.rb
+++ spec/debitcard_validator_spec.rb
@@ -11,35 +11,35 @@ module ActiveModel
         TestCreditcard.reset_callbacks(:validate)
       end
 
-      it "when debitcard number" do
+      it "returns error message when debitcard number" do
         TestCreditcard.validates :number, debitcard: true
         creditcard = TestCreditcard.new(number: '4153141111111111')
         expect(creditcard.valid?).to be_falsey
         expect(creditcard.errors[:number]).to eq(["is debitcard."])
       end
 
-      it "when creditcard string" do
+      it "should be valid when creditcard string" do
         TestCreditcard.validates :number, debitcard: true
         creditcard = TestCreditcard.new(number: '4111111111111111')
         expect(creditcard.valid?).to be_truthy
         expect(creditcard.errors[:number]).to eq([])
       end
 
-      it "when creditcard number (integer)" do
+      it "should be valid when creditcard number (integer)" do
         TestCreditcard.validates :number, debitcard: true
         creditcard = TestCreditcard.new(number: 4111111111111111)
         expect(creditcard.valid?).to be_truthy
         expect(creditcard.errors[:number]).to eq([])
       end
 
-      it "when creditcard number is nil" do
+      it "should be valid when creditcard number is nil" do
         TestCreditcard.validates :number, debitcard: true
         creditcard = TestCreditcard.new(number: nil)
         expect(creditcard.valid?).to be_truthy
         expect(creditcard.errors[:number]).to eq([])
       end
 
-      it "when prepaidcard number" do
+      it "returns error message when prepaidcard number" do
         TestCreditcard.validates :number, debitcard: true
         creditcard = TestCreditcard.new(number: '4284851111111111')
         expect(creditcard.valid?).to be_falsey
